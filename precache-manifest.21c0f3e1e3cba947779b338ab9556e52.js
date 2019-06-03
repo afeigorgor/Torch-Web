8de1ff76c66d231c356fbfedb3c5085ce1f84c42
Travis Automatically Bot (from Travis CI) <jiduye@gmail.com>
2019-06-03 15:30:05 +0000

    Deploy Indexyz/Torch-Web to github.com/Indexyz/Torch-Web.git:gh-pages

---
+++ precache-manifest.21c0f3e1e3cba947779b338ab9556e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62421b74dd15c2c2d5aac9436106170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb25064dd639cfe88dd0",
+    "url": "/static/css/2.a67319c4.chunk.css"
+  },
+  {
+    "revision": "9b7baed66041310e2ab0",
+    "url": "/static/css/main.81f517e2.chunk.css"
+  },
+  {
+    "revision": "fb25064dd639cfe88dd0",
+    "url": "/static/js/2.67f315cb.chunk.js"
+  },
+  {
+    "revision": "9b7baed66041310e2ab0",
+    "url": "/static/js/main.93a3c2fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
